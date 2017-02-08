e5b9c9c6953fd1ad190445dd93b850f153a725f0
Kris Rogo <krisrogos@gmail.com>
2017-02-08 15:04:28 +0000

    Final commit
    Unity was not a good choice, end this nightmare and switch to Unreal
    Engine

---
+++ Assets/Scripts/LevelRotationController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelRotationController : MonoBehaviour {
+
+    public float m_Speed = 1f;
+    private float rotator;
+
+    // Use this for initialization
+    void Start () {
+        rotator = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!Application.isMobilePlatform)
+            rotator = m_Speed * Input.GetAxis("Wheel Rotation");
+        else
+            rotator = m_Speed * Input.acceleration.x;
+
+        transform.RotateAround(new Vector3(0f, 0f, 0f), new Vector3(0f, 0f, 1f), rotator);
+	}
+}
